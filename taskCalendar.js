5a82fda1745fa0734e42dd4db7e3707770e11a96
unija28 <cabellunesa@gmail.com>
2025-05-20 17:24:19 +0800

    Create taskCalendar.js

---
+++ taskCalendar.js
@@ -0,0 +1,26 @@
+let day = prompt("Enter a day of the week");
+
+switch (day) {
+    
+    case "Monday":
+        
+        console.log("Finish all your Requirments");
+        break;
+        
+    case "Tuesday":
+        
+        console.log("Start your yoga exercise");
+        break;
+        
+    case "Wednesday":
+        
+        console.log("It's your rest day");
+        break;
+        
+    case "Thursday":
+        
+        console.log("Do your activity in OOP");
+        break;
+    default:
+        console.log("It's Weekends");
+}
